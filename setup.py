64e93f122301ef0a9e96220fbf0c154570d4c61d
Clem Dep <dev@depruneaux.com>
2020-08-04 17:43:36 -0500

    added setup.py and fixed main script

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="hash-generator-cli-clemdep",
+    version="0.0.1",
+    author="Clem Dep",
+    author_email="dev@depruneaux.com",
+    description="A small CLI hash generator based on python hashlib",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/clemdep/hash-generator-cli",
+    entry_points={
+        "console_scripts": [
+            "hash_generator_cli=hash_generator_cli:run"
+        ]
+    },
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)

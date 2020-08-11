25cf573df53d9ff16f3825fc10947d8e8205b38a
Clem Dep <dev@depruneaux.com>
2020-08-11 08:42:44 -0500

    PEP8 changes

--- hash_generator_cli/tests/test_string_hasher.py
+++ hash_generator_cli/tests/test_string_hasher.py
@@ -1,17 +1,19 @@
-import sys, os
+import string_hasher as s
+import unittest
+import sys
+import os
 testdir = os.path.dirname(__file__)
 srcdir = '../lib'
 sys.path.insert(0, os.path.abspath(os.path.join(testdir, srcdir)))
 
-import unittest
-import string_hasher as s
 
 class TestStringHasher(unittest.TestCase):
-    
+
     def setUp(self):
         self.string_hasher = s.StringHasher("hello", "md5")
 
     def test_hasher(self):
         # TODO Testing with a variety of hashes and strings
-        self.assertEqual(self.string_hasher.hasher(), "5d41402abc4b2a76b9719d911017c592")
-
+        self.assertEqual(
+            self.string_hasher.hasher(),
+            "5d41402abc4b2a76b9719d911017c592")
